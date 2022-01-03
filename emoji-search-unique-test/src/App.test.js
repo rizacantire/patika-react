6b017d432f02248efd63f7b9c04635305355a2c1
rza35 <riza_tire@hotmail.com>
2022-01-03 16:50:13 +0300

    copy test

--- emoji-search-unique-test/src/App.test.js
+++ emoji-search-unique-test/src/App.test.js
@@ -1,24 +1,40 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import '@testing-library/jest-dom'
 import App from "./App";
+import ClipboardJS from "clipboard";
 
 describe("Emoji List",()=>{
-    let input;
+   
+    let input,items;
     beforeEach(()=>{
+      
         render(<App/>)
+        input = document.querySelector('.search')
+        items = screen.getAllByText("Click to copy emoji")
     })
     test("Emoji Loading...",()=>{
         render(<App />)
         const items = screen.getAllByText("Click to copy emoji");
         expect(items.length).toBeGreaterThan(0)
-     
     })
 
-    test("Emoji Searching",()=>{
-        let d =document.querySelector(".component-search-input")
-        screen.queryAllBy(".component-search-input")
-        console.log(d.innerHTML);
-        
-    })
+    test('Searching... ', () => {    
+    expect(input).not.toBeNull()
+    userEvent.type(input,"coffee")
+    
+    expect(items.length>0).toBeTruthy()
+
+  })
+
+  test("Copying...",()=>{
+ 
+    const clipboard = new ClipboardJS('.copy-to-clipboard');
+    userEvent.click(items[2])
+    const copyItem = clipboard.clipboardAction.text
+ 
+    expect(copyItem).toBeInTheDocument
+    
+  })
 })
--- emoji-search-unique-test/src/components/SearchInput/SearchInput.js
+++ emoji-search-unique-test/src/components/SearchInput/SearchInput.js
@@ -16,7 +16,7 @@ export default class SearchInput extends PureComponent {
     return (
       <div className="component-search-input">
         <div>
-          <input onChange={this.handleChange} />
+          <input  className="search" onChange={this.handleChange} />
         </div>
       </div>
     );
